95476340535c651af862ffa3d1486e5fd438b03f
Garth Calkwood <garth.calkwood@gmail.com>
2021-08-20 13:27:00 -0500

    Added search feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [tempos, settempos] = useState([
+    {
+      name: "Smash Mouth - All Star",
+      tempo: 104
+    },
+    {
+      name: "Rick Astley - Never Gonna Give You Up",
+      tempo: 113
+    },
+    {
+      name: "Gunther & The Sunshine Girls - Ding Dong Song",
+      tempo: 132
+    }
+  ]) 
+  const [search, setSearch] = useState('')
+
+  const handleSearch = event => setSearch(event.target.value)
+
+  const filteredTempos = tempos.filter(
+    tempo => tempo.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div>
       <h1>Metronome</h1>
+      <div>
+        <h2>Saved Tempos</h2>
+        <Search value={search} onChange={handleSearch}/>
+        {filteredTempos.map(tempo => 
+          <div> {tempo.name}: {tempo.tempo} BPM </div>
+        )}
+      </div>
     </div>
   );
 }
