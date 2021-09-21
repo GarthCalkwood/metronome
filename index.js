853bf6b3455dee4021f61f396aa354d98c6fbaef
Garth Calkwood <garth.calkwood@gmail.com>
2021-09-21 15:29:43 -0500

    Add morgan for logging requests

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
+
+morgan.token("tempo", (req, res) => JSON.stringify(req.body));
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :tempo"));
 
 let tempos = [
   {
