324dbc9576bf93b3feb79c7e887635902ccc6601
Garth Calkwood <garth.calkwood@gmail.com>
2021-11-04 17:27:04 -0500

    Refactor routes
    Refactor the tempo routes to use async/await instead of promises to
    improve readablility. Also add several semicolons that were missing.

--- controllers/tempos.js
+++ controllers/tempos.js
@@ -1,62 +1,65 @@
 const tempoRouter = require("express").Router();
 const Tempo = require("../models/tempo");
 
-tempoRouter.get("/", (req, res) => {
-  Tempo.find({}).then(results => {
-    res.json(results);
-  })
-})
+tempoRouter.get("/", async (req, res) => {
+  const results = await Tempo.find({});
+  res.json(results);
+});
 
-tempoRouter.get("/:id", (req, res, next) => {
-  Tempo.findById(req.params.id)
-    .then(tempo => {
-      res.json(tempo)
-    })
-    .catch(error => next(error))
-})
+tempoRouter.get("/:id", async (req, res, next) => {
+  try {
+    const tempo = await Tempo.findById(req.params.id);
+    res.json(tempo);
+  } catch (err) {
+    next(err);
+  };
+});
 
-tempoRouter.delete("/:id", (req, res, next) => {
-  Tempo.findByIdAndDelete(req.params.id)
-    .then(results => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
-})
+tempoRouter.delete("/:id", async (req, res, next) => {
+  try {
+    await Tempo.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  };
+});
 
-tempoRouter.post("/", (req, res, next) => {
+tempoRouter.post("/", async (req, res, next) => {
   const body = req.body;
 
   if (!body.name) {
     return res.status(400).json({
       error: "name missing",
-    })
-  }
+    });
+  };
 
   const newTempo = new Tempo({
     name: body.name,
     tempo: body.tempo ? body.tempo : 120,
   });
 
-  newTempo.save()
-    .then(result => {
-      res.json(newTempo);
-    })
-    .catch(error => next(error))
-})
+  try {
+    await newTempo.save();
+    res.json(newTempo);
+  } catch (err) {
+    next(err);
+  };
+});
 
-tempoRouter.put("/:id", (req, res, next) => {
+tempoRouter.put("/:id", async (req, res, next) => {
   const body = req.body;
 
   const newTempo = {
     name: body.name,
     tempo: body.tempo,
+  };
+
+  try {
+    const updatedTempo = await Tempo.findByIdAndUpdate(req.params.id, newTempo, {new: true});
+    res.json(updatedTempo);
+  } catch (err) {
+    next(err);
   }
+});
 
-  Tempo.findByIdAndUpdate(req.params.id, newTempo, {new: true})
-    .then(updatedTempo => {
-      res.json(updatedTempo)
-    })
-    .catch(error => next(error))
-})
-
-module.exports = tempoRouter
+module.exports = tempoRouter;
