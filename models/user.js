8b571ae250f6c74212485b0f8738e423f407bc47
Garth Calkwood <garth.calkwood@gmail.com>
2021-11-13 14:12:11 -0600

    Add user schema

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  tempos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tempo"
+    }
+  ],
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
